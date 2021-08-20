c63d54c49dea2015043e61825bc258a169289dfc
rahul <rahulsrmacs@gmail.com>
2021-08-20 12:47:01 +0530

    Added two models

---
+++ Models/PartyContact.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoApplication.Models
+{
+    public class PartyContact
+    {
+        public int Id { get; set; }
+        public string Address { get; set; }
+        public int PartyIdentityId { get; set; }
+        public DateTime CreationDateTime { get; set; }
+        public PartyIdentity PartyIdentity { get; set; }
+
+    }
+}
