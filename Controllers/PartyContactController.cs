2d1424e7e950b38f989941ae6ae42a7250c15b07
rahul <rahulsrmacs@gmail.com>
2021-08-20 13:34:22 +0530

    Added endpoints in controllers

---
+++ Controllers/PartyContactController.cs
@@ -0,0 +1,30 @@
+﻿using DemoApplication.DBContexts;
+using DemoApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoApplication.Controllers
+{
+    public class PartyContactController : Controller
+    {
+        private MyDBContext myDbContext;
+
+        public PartyContactController(MyDBContext context)
+        {
+            myDbContext = context;
+        }
+
+        [HttpGet]
+        public IList<PartyContact> Get()
+        {
+            return (this.myDbContext.PartyContact.ToList());
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/PartyIdentityController.cs
@@ -0,0 +1,30 @@
+﻿using DemoApplication.DBContexts;
+using DemoApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoApplication.Controllers
+{
+    public class PartyIdentityController : Controller
+    {
+        private MyDBContext myDbContext;
+
+        public PartyIdentityController(MyDBContext context)
+        {
+            myDbContext = context;
+        }
+
+        [HttpGet]
+        public IList<PartyIdentity> Get()
+        {
+            return (this.myDbContext.PartyIdentity.ToList());
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
