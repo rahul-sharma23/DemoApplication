b46d681c9d63397d14c0093994f94889081317cb
rahul <rahulsrmacs@gmail.com>
2021-08-20 13:02:54 +0530

    Added MySQL connection parameters

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,8 @@
+using DemoApplication.DBContexts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,10 @@ namespace DemoApplication
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            string mySqlConnectionStr = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContextPool<MyDBContext>(options => options.UseMySql(mySqlConnectionStr, ServerVersion.AutoDetect(mySqlConnectionStr)));
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
